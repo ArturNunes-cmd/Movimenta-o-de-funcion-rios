31d995fca7b6e0acd4ee5f6cbd69d6de6d23cb28
Artur <Arturduartebsb@outlook.com>
2025-10-27 22:06:10 -0300

    primeiro commit

---
+++ index.py
@@ -0,0 +1,51 @@
+funcionario = []
+
+while True:
+    print("\nMENU".center(15))
+    print("1 - Cadastrar funcionario")
+    print("2 - Visualizar todos os funcionarios")
+    print("3 - Buscar por nome")
+    print("4 - Calcular media salarial")
+    print("5 - Sair")
+
+    opcao = int(input("Digite a opção desejada:"))
+
+    if opcao == 1:
+        print("\nCadastrar funcionario")
+        continuar = "S"
+        while continuar == "S": 
+            nome = input("Digite o nome do funcionario:").upper()
+            salario = float(input("Digite o salário do funcionario(não use vírgula):"))
+            funcionario.append([nome, salario])
+            continuar = input("Deseja adicionar outro funcionario(S/N): ").upper()
+            if continuar == "N":
+                break
+
+    elif opcao == 2:
+        print("\nVisualizar todos os funcionarios:")
+        if not funcionario:
+            print("Nenhum funcionario foi cadastrado!")
+        else:
+            for f in funcionario:
+                print(f"Nome: {f[0]}, Salário: {f[1]}")
+
+    elif opcao == 3:
+        print("\nBuscar por nome:")
+        nome_busca = input("Digite o nome do funcionario:").upper()
+        encontrado = False
+        for f in funcionario:
+            if f[0] == nome_busca:
+                print(f"Nome: {f[0]}, Salário: {f[1]}")
+                encontrado = True
+                break
+        if not encontrado:
+            print("Funcionario não encontrado!")
+
+    elif opcao == 4:
+        print("\nCalcular média salarial:")
+
+    elif opcao == 5:
+        print("\nSair")
+        break
+    else:
+        print("Opção inválida. Tente novamente.")
